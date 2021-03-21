a7864c96c5d5c578c6c48217a72fa5bcd5bb6f9b
MdNoorr <nurmuhammadd2001@gmail.com>
2021-03-21 12:23:01 +0600

    Complete

---
+++ src/components/Bookings/Bookings.js
@@ -0,0 +1,45 @@
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../App";
+
+const Bookings = () => {
+  const [bookings, setBookings] = useState([]);
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+  // useEffect(() => {
+  //         fetch('http://localhost:5000/bookings?email='+loggedInUser.email)
+  //         .then(res => res.json())
+  //         .then(data => setBookings(data));
+  //     }, [])
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/bookings?email=${loggedInUser.email}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+    })
+      // fetch('http://localhost:5000/bookings?email='+loggedInUser.email, {
+
+      // })
+      .then((res) => res.json())
+      .then((data) => setBookings(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>You have: {bookings.length} bookings</h3>
+      {bookings.map((book) => (
+        <div style={{ marginBottom: "20px", listStyleType: 'none'}} >
+          <li key={book._id}>
+            {book.name} from:{" "}
+            {new Date(book.checkIn).toDateString("dd/MM/yyyy")} to:{" "}
+            {new Date(book.checkOut).toDateString("dd/MM/yyyy")}
+          </li>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Bookings;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ-yde8gLXhRGDBpsq-ujp1MEYC9gpQ48",
+  authDomain: "burj-al-arab-c8679.firebaseapp.com",
+  projectId: "burj-al-arab-c8679",
+  storageBucket: "burj-al-arab-c8679.appspot.com",
+  messagingSenderId: "59990469233",
+  appId: "1:59990469233:web:94055f84944e1910dec9d0",
+  measurementId: "G-FD2SJKR4W8"
+};
+
+export default firebaseConfig;
